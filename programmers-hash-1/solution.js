c7e569e2fc09527fbe68f1308eeefbbe70901119
IlYeongJeong <developjiy@gmail.com>
2020-10-14 21:55:15 +0900

    프로그래머스 코딩테스트 연습

---
+++ programmers-hash-1/solution.js
@@ -0,0 +1,10 @@
+const solution = (participant, completion) => {
+  participant.sort();
+  completion.sort();
+  
+  for (let i in participant) {
+    if (participant[i] !== completion[i]) {
+      return participant[i]
+    };
+  }
+};
