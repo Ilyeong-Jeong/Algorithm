408a344d924f784238b345ecdc591c74f7b0f17b
ilyeongJeong <developjiy@gmail.com>
2021-03-02 15:04:23 +0900

    ref #1 프로그래머스 완전탐색 1번 문제 solution 추가

--- programmers-exhaustive-search-1/solution.js
+++ programmers-exhaustive-search-1/solution.js
@@ -1,4 +1,32 @@
-function solution(answers) {
+function solutionFirst(answers) {
+  const guessFirst  = [1, 2, 3, 4, 5];
+  const guessSecond = [2, 1, 2, 3, 2, 4, 2, 5];
+  const guessThird  = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  const answerFirst = answers.filter((v, i) => {
+    return v === guessFirst[i % guessFirst.length]
+  }).length
+
+  const answerSecond = answers.filter((v, i) => {
+    return v === guessSecond[i % guessSecond.length]
+  }).length
+
+  const answerThird = answers.filter((v, i) => {
+    return v === guessThird[i % guessThird.length]
+  }).length
+
+  const answerTotal = [answerFirst, answerSecond, answerThird]
+
+  const max = Math.max.apply(null, answerTotal);
+
+  const answer = answerTotal.map((v, i) => {
+    if (v === max) return i + 1;
+  }).filter(x => x)
+
+  return answer;
+}
+
+function solutionSecond(answers) {
   let answer = [];
 
   const guessFirst  = [1, 2, 3, 4, 5];
@@ -18,7 +46,7 @@ function solution(answers) {
     score.push(count);
   }
 
-  let max = Math.max.apply(null, score);
+  const max = Math.max.apply(null, score);
   let idx = score.indexOf(max);
 
   while (idx !== -1) {
